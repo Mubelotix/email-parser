da5e40119c3a00064be56600f4fd860e2fb4d8d1
Mubelotix <mubelotix@gmail.com>
2020-06-29 16:24:08 +0200

    add obsolete rfc tolerance

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,5 @@
 use nom::IResult;
-use nom::bytes::complete::{tag, take_while1};
+use nom::bytes::complete::{tag, take_while, take_while1};
 use nom::sequence::tuple;
 use nom::combinator::opt;
 use nom::multi::many0;
@@ -55,13 +55,33 @@ fn field_name(data: &[u8]) -> IResult<&[u8], &[u8]> {
     take_while1(is_ftext)(data)
 }
 
+fn header_separator(data: &[u8]) -> IResult<&[u8], &[u8]> {
+    let mut idx = 0;
+    while idx < data.len() && (data[idx] == b' ' || data[idx] == b'\t') {
+        idx += 1;
+    }
+    if idx < data.len() && data[idx] == b':' {
+        idx += 1;
+        Ok((&data[idx..], &data[..idx]))
+    } else {
+        Err(nom::Err::Failure((&data[idx..],nom::error::ErrorKind::CrLf)))
+    }
+}
+
 fn optionnal_field(data: &[u8]) -> IResult<&[u8], (&[u8], &[u8])> {
-    let result = tuple((field_name, tag(":"), unstructured_header_value, tag("\r\n")))(data)?;
+    let result = tuple((field_name, header_separator, unstructured_header_value, tag("\r\n")))(data)?;
     Ok((result.0, ((result.1).0, (result.1).2)))
+}
+fn optionnal_field_with_separator(data: &[u8]) -> IResult<&[u8], (&[u8], &[u8], &[u8])> {
+    let result = tuple((field_name, header_separator, unstructured_header_value, tag("\r\n")))(data)?;
+    Ok((result.0, ((result.1).0, (result.1).1, (result.1).2)))
 }
 
 fn fields(data: &[u8]) -> IResult<&[u8], Vec<(&[u8], &[u8])>> {
     many0(optionnal_field)(data) // TODO obs-fields
+}
+fn fields_with_separators(data: &[u8]) -> IResult<&[u8], Vec<(&[u8], &[u8], &[u8])>> {
+    many0(optionnal_field_with_separator)(data) // TODO obs-fields
 }
 
 fn body(data: &[u8]) -> IResult<&[u8], &[u8]> {
@@ -91,10 +111,23 @@ pub fn parse_message(data: &[u8]) -> Result<(Vec<(&[u8], &[u8])>, Option<&[u8]>)
     }
 }
 
+/// Like `parse_message()` but headers are a tuple of three values: (name, separator, value).  
+///   
+/// Example: `"HeaderName  : header value\r\n"` gives `("HeaderName", "  :", "header value")`
+pub fn parse_message_with_separators(data: &[u8]) -> Result<(Vec<(&[u8], &[u8], &[u8])>, Option<&[u8]>), ()> {
+    if let Ok(result) = tuple((fields_with_separators, opt(tuple((tag("\r\n"), body)))))(data) {
+        Ok(((result.1).0, (result.1).1.map(|b| b.1)))
+    } else {
+        Err(())
+    }
+}
+
 #[cfg(test)]
 mod test {
+    use super::{parse_message, header_separator};
+
     #[test]
-    fn header_test() {
+    fn main_test() {
         use super::parse_message;
         let mail = include_bytes!("../mail.txt");
         
@@ -105,5 +138,21 @@ mod test {
             println!("{}:{}", std::str::from_utf8(name).unwrap(), std::str::from_utf8(value).unwrap())
         }
         println!("{}", std::str::from_utf8(body.unwrap()).unwrap())
+    }
+
+    #[test]
+    fn secondary_test() {
+        let mail = b"A: X\r\nB : Y\t\r\n\tZ  \r\n\r\n C \r\nD \t E\r\n\r\n\r\n";
+        
+        let (headers, _body) = parse_message(mail).unwrap();
+        
+        assert_eq!(headers.len(), 2);
+    }
+
+    #[test]
+    fn test_separator() {
+        header_separator(b"   :  ").unwrap();
+        header_separator(b"   :").unwrap();
+        header_separator(b"   ").unwrap_err();
     }
 }
