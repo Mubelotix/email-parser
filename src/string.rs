d9fcffb237c4ff85e1179df5fbc4dd47c4583660
Mubelotix <mubelotix@gmail.com>
2020-11-07 18:22:37 +0100

    Unsafe optimization

--- src/string.rs
+++ src/string.rs
@@ -80,24 +80,20 @@ impl<'a> std::ops::AddAssign for String<'a> {
                         *data1 = data2;
                         return;
                     }
-                    if let (Some(first1), Some(last1), Some(first2), Some(last2)) =
-                        (data1.first(), data1.last(), data2.first(), data2.last())
-                    {
-                        // if the two references are consecutive in memory, we create a third reference containing them
-                        unsafe {
-                            let first1 = first1 as *const u8;
-                            let last1 = last1 as *const u8;
-                            let first2 = first2 as *const u8;
-                            let last2 = last2 as *const u8;
-                            if last1 as usize + std::mem::size_of::<u8>() == first2 as usize {
-                                // this is what guarantee safety
-                                let slice = std::slice::from_raw_parts(
-                                    first1,
-                                    last2 as usize - first1 as usize + 1,
-                                );
-                                *data1 = slice;
-                                return;
-                            }
+                    // if the two references are consecutive in memory, we create a third reference containing them
+                    unsafe {
+                        let first1 = data1.get_unchecked(0) as *const u8;
+                        let last1 = data1.get_unchecked(data1.len() - 1) as *const u8;
+                        let first2 = data2.get_unchecked(0) as *const u8;
+                        let last2 = data2.get_unchecked(data2.len() - 1) as *const u8;
+                        if last1 as usize + std::mem::size_of::<u8>() == first2 as usize {
+                            // this is what guarantee safety
+                            let slice = std::slice::from_raw_parts(
+                                first1,
+                                last2 as usize - first1 as usize + 1,
+                            );
+                            *data1 = slice;
+                            return;
                         }
                     }
                 }
