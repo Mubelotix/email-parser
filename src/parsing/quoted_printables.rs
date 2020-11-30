9b712cb5d3545ae5cbf43fcc022f43941d4c863d
Mubelotix <mubelotix@gmail.com>
2020-11-30 11:08:03 +0100

    Optimize encoding

--- src/parsing/quoted_printables.rs
+++ src/parsing/quoted_printables.rs
@@ -5,38 +5,37 @@ fn litteral_repr_possible(c: u8) -> bool {
 
 #[allow(clippy::if_same_then_else)]
 pub fn encode_qp(input: String) -> String {
-    let mut output = Vec::new();
-    let mut bytes = input.bytes().peekable();
+    let mut input = input.into_bytes();
     let mut line_lenght = 0;
+    let mut idx = 0;
 
-    while let Some(byte) = bytes.next() {
+    while let Some(byte) = input.get(idx).copied() {
         if line_lenght >= 72 { // 72 because in the worst case we add 3 chars + 1 equal sign -> 76
             // Fifth rule
-            output.push(b'=');
-            output.push(b'\r');
-            output.push(b'\n');
+            input.insert(idx, b'\r');
+            input.insert(idx, b'\n');
+            input.insert(idx, b'=');
+            idx += 3;
             line_lenght = 0;
         }
 
-        line_lenght = if litteral_repr_possible(byte) {
+        if litteral_repr_possible(byte) {
             // Second rule
-            output.push(byte);
-            line_lenght + 1
+            idx += 1;
+            line_lenght += 1;
         } else if (byte == 9 || byte == 32)
-            && bytes
-                .peek()
+            && input
+                .get(idx + 1)
                 .map(|c| c != &b'\r')
                 .unwrap_or(false)
         {
             // Third rule
-            output.push(byte);
-            line_lenght + 1
-        } else if byte == b'\r' && bytes.peek() == Some(&b'\n') {
+            idx += 1;
+            line_lenght += 1;
+        } else if byte == b'\r' && input.get(idx + 1) == Some(&b'\n') {
             // Fourth rule
-            bytes.next();
-            output.push(b'\r');
-            output.push(b'\n');
-            0
+            idx += 2;
+            line_lenght = 0;
         } else {
             // First rule
 
@@ -48,13 +47,17 @@ pub fn encode_qp(input: String) -> String {
                 }
             }
 
-            output.push(b'=');
-            output.push(to_hex((byte - byte % 16) / 16));
-            output.push(to_hex(byte % 16));
-            line_lenght + 3
-        }
+            input.remove(idx);
+            input.insert(idx, to_hex(byte % 16));
+            input.insert(idx, to_hex((byte - byte % 16) / 16));
+            input.insert(idx, b'=');
+
+            idx += 3;
+            line_lenght += 3;
+        };
     }
-    unsafe { String::from_utf8_unchecked(output) }
+
+    unsafe { String::from_utf8_unchecked(input) }
 }
 
 #[cfg(test)]
@@ -63,7 +66,7 @@ mod tests {
 
     #[test]
     fn encode() {
-        println!("{:?}", encode_qp("This was a triumph. Il était une fois...\r\nAnd voilà ! \r\nSimon".to_string()));
-        println!("{:?}", encode_qp("Now's the time for all folk to come to the aid of their country. Wtf this sentence is not long enough to test line-lenght limit.".to_string()));
+        assert_eq!("This was a triumph. Il =C3=A9tait une fois...\r\nAnd voil=C3=A0 !=20\r\nSimon", encode_qp("This was a triumph. Il était une fois...\r\nAnd voilà ! \r\nSimon".to_string()));
+        assert_eq!("Now\'s the time for all folk to come to the aid of their country. Wtf thi=\n\rs sentence is not long enough to test line-lenght limit.", encode_qp("Now's the time for all folk to come to the aid of their country. Wtf this sentence is not long enough to test line-lenght limit.".to_string()));
     }
 }
