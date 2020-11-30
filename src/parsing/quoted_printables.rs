0e94c310d69571ea04ea3ffb1e845685681f487a
Mubelotix <mubelotix@gmail.com>
2020-11-30 14:09:08 +0100

    Add decoding

--- src/parsing/quoted_printables.rs
+++ src/parsing/quoted_printables.rs
@@ -1,20 +1,22 @@
+use std::time::SystemTimeError;
+
 // Second rule of the encoding
 fn litteral_repr_possible(c: u8) -> bool {
     (c >= 33 && c <= 60) || (c >= 62 && c <= 126)
 }
 
 #[allow(clippy::if_same_then_else)]
-pub fn encode_qp(input: String) -> String {
-    let mut input = input.into_bytes();
+pub fn encode_qp(mut data: Vec<u8>) -> Vec<u8> {
     let mut line_lenght = 0;
     let mut idx = 0;
 
-    while let Some(byte) = input.get(idx).copied() {
-        if line_lenght >= 72 { // 72 because in the worst case we add 3 chars + 1 equal sign -> 76
+    while let Some(byte) = data.get(idx).copied() {
+        if line_lenght >= 72 {
+            // 72 because in the worst case we add 3 chars + 1 equal sign -> 76
             // Fifth rule
-            input.insert(idx, b'\r');
-            input.insert(idx, b'\n');
-            input.insert(idx, b'=');
+            data.insert(idx, b'\n');
+            data.insert(idx, b'\r');
+            data.insert(idx, b'=');
             idx += 3;
             line_lenght = 0;
         }
@@ -24,15 +26,12 @@ pub fn encode_qp(input: String) -> String {
             idx += 1;
             line_lenght += 1;
         } else if (byte == 9 || byte == 32)
-            && input
-                .get(idx + 1)
-                .map(|c| c != &b'\r')
-                .unwrap_or(false)
+            && data.get(idx + 1).map(|c| c != &b'\r').unwrap_or(false)
         {
             // Third rule
             idx += 1;
             line_lenght += 1;
-        } else if byte == b'\r' && input.get(idx + 1) == Some(&b'\n') {
+        } else if byte == b'\r' && data.get(idx + 1) == Some(&b'\n') {
             // Fourth rule
             idx += 2;
             line_lenght = 0;
@@ -47,17 +46,72 @@ pub fn encode_qp(input: String) -> String {
                 }
             }
 
-            input.remove(idx);
-            input.insert(idx, to_hex(byte % 16));
-            input.insert(idx, to_hex((byte - byte % 16) / 16));
-            input.insert(idx, b'=');
+            data.remove(idx);
+            data.insert(idx, to_hex(byte % 16));
+            data.insert(idx, to_hex((byte - byte % 16) / 16));
+            data.insert(idx, b'=');
 
             idx += 3;
             line_lenght += 3;
         };
     }
 
-    unsafe { String::from_utf8_unchecked(input) }
+    data
+}
+
+pub fn decode_qp(mut data: Vec<u8>) -> String {
+    let mut idx = 0;
+
+    while let Some(byte) = data.get(idx).copied() {
+        if litteral_repr_possible(byte) || byte == b' ' || byte == b'\t' {
+            idx += 1;
+        } else if byte == b'=' {
+            if data.get(idx + 1) == Some(&b'\r') && data.get(idx + 2) == Some(&b'\n') {
+                data.remove(idx);
+                data.remove(idx);
+                data.remove(idx);
+            } else if data.len() > idx + 2 {
+                let first = data.remove(idx + 1);
+                let second = data.remove(idx + 1);
+
+                fn from_hex(n: u8) -> Option<u8> {
+                    match n {
+                        b'0'..=b'9' => Some(n - b'0'),
+                        b'A'..=b'F' => Some(10 + n - b'A'),
+                        b'a'..=b'f' => Some(10 + n - b'a'),
+                        _ => None,
+                    }
+                }
+
+                if let (Some(first), Some(second)) = (from_hex(first), from_hex(second)) {
+                    data[idx] = first * 16 + second;
+                    idx += 1;
+                } else {
+                    data[idx] = 189;
+                    data.insert(idx, 191);
+                    data.insert(idx, 239);
+                    idx += 3;
+                }
+            } else {
+                idx += 1;
+            }
+        } else if byte == b'\r' && data.get(idx + 1) == Some(&b'\n') {
+            idx += 2;
+        } else {
+            data[idx] = 189;
+            data.insert(idx, 191);
+            data.insert(idx, 239);
+            idx += 3;
+        }
+    }
+
+    match String::from_utf8(data) {
+        Ok(s) => s,
+        Err(e) => {
+            let s = String::from_utf8_lossy(e.as_bytes());
+            s.into()
+        }
+    }
 }
 
 #[cfg(test)]
@@ -66,7 +120,29 @@ mod tests {
 
     #[test]
     fn encode() {
-        assert_eq!("This was a triumph. Il =C3=A9tait une fois...\r\nAnd voil=C3=A0 !=20\r\nSimon", encode_qp("This was a triumph. Il était une fois...\r\nAnd voilà ! \r\nSimon".to_string()));
-        assert_eq!("Now\'s the time for all folk to come to the aid of their country. Wtf thi=\n\rs sentence is not long enough to test line-lenght limit.", encode_qp("Now's the time for all folk to come to the aid of their country. Wtf this sentence is not long enough to test line-lenght limit.".to_string()));
+        assert_eq!(
+            b"This was a triumph. Il =C3=A9tait une fois...\r\nAnd voil=C3=A0 !=20\r\nSimon"
+                .to_vec(),
+            encode_qp(
+                "This was a triumph. Il était une fois...\r\nAnd voilà ! \r\nSimon"
+                    .to_string()
+                    .into_bytes()
+            )
+        );
+        assert_eq!(b"Now\'s the time for all folk to come to the aid of their country. Wtf thi=\r\ns sentence is not long enough to test line-lenght limit.".to_vec(), encode_qp("Now's the time for all folk to come to the aid of their country. Wtf this sentence is not long enough to test line-lenght limit.".to_string().into_bytes()));
+    }
+
+    #[test]
+    fn decode() {
+        assert_eq!(
+            "This was a triumph. Il était une fois...\r\nAnd voilà ! \r\nSimon".to_string(),
+            decode_qp(
+                "This was a triumph. Il =C3=A9tait une fois...\r\nAnd voil=C3=A0 !=20\r\nSimon"
+                    .to_string()
+                    .into_bytes()
+            )
+        );
+        assert_eq!("Now's the time for all folk to come to the aid of their country. Wtf this sentence is not long enough to test line-lenght limit.".to_string(), decode_qp("Now\'s the time for all folk to come to the aid of their country. Wtf thi=\r\ns sentence is not long enough to test line-lenght limit.".to_string().into_bytes()));
+        assert_eq!("Invalid bytes�Q�\u{1a}�".to_string(), decode_qp("Invalid bytes=95=51=FF=1A=FA".to_string().into_bytes()));
     }
 }
